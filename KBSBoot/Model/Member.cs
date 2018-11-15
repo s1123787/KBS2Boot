2b010e258a8fa4ac419470e40a815d5cff94c013
Mick Nieweg (student) <s1123787@student.windesheim.nl>
2018-11-15 13:44:40 +0100

    Adding ContextDB to project for accessing database

---
+++ KBSBoot/Model/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBSBoot.Model
+{
+    class Member
+    {
+        public int memberId { get; set; }
+        public string memberName { get; set; }
+        public int memberAccessLevelId { get; set; }
+        public int memberRowLevelId { get; set; }
+        public DateTime memberSubscribedUntill { get; set; }
+
+    }
+}
