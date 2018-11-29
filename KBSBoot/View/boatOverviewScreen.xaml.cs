412d651c0a17d8987914880cf8a3b7c15283bb0d
Mika <mikavandenbrink@outlook.com>
2018-11-29 14:36:38 +0100

    Added Filterbutton above Boatoverview
    it has no functionality at this point

--- KBSBoot/View/boatOverviewScreen.xaml.cs
+++ KBSBoot/View/boatOverviewScreen.xaml.cs
@@ -28,7 +28,8 @@ namespace KBSBoot.View
             this.FullName = FullName;
             InitializeComponent();
             boatList.ItemsSource = LoadCollectionData();
-
+            Bootnamen.ItemsSource = LoadBoatNameSelection();
+            Bootplekken.ItemsSource = LoadBoatSeatsSelection();
         }
 
         private List<Boat> LoadCollectionData()
@@ -64,12 +65,60 @@ namespace KBSBoot.View
                         boatOutOfServiceString = (b.boatOutOfService == 0) ? "nee" : "ja"
                     });
                 }
-
-
                 return boats;
             }
         }
+        private List<Boat> LoadBoatNameSelection()
+        {
+            List<Boat> boatnames = new List<Boat>();
+            using (var context = new BootDB())
+            {
+                var tableData = (from b in context.Boats
+                                 join bt in context.BoatTypes
+                                 on b.boatTypeId equals bt.boatTypeId
+                                 select new
+                                 {
+                                     boatTypeName = bt.boatTypeName
+                                 });
+                
+                foreach(var b in tableData)
+                {
+                    boatnames.Add(new Boat()
+                    {
+                        //Adds boatnames to list
+                        boatTypeName = b.boatTypeName
+                    });
+                }
+            }
+            List<Boat> DistinctBoatNames = new List<Boat>();
+            DistinctBoatNames = boatnames.GroupBy(elem => elem.boatTypeName).Select(g => g.First()).ToList();
+            return DistinctBoatNames;
+        }
+        private List<Boat> LoadBoatSeatsSelection()
+        {
+            List<Boat> boatseats = new List<Boat>();
+            using (var context = new BootDB())
+            {
+                var tableData = (from b in context.Boats
+                                 join bt in context.BoatTypes
+                                 on b.boatTypeId equals bt.boatTypeId
+                                 select new
+                                 {
+                                     boatAmountSpaces = bt.boatAmountSpaces
+                                 });
 
+                foreach (var b in tableData)
+                {
+                    boatseats.Add(new Boat()
+                    {
+                        boatAmountSpaces = b.boatAmountSpaces
+                    });
+                }
+            }
+            List<Boat> DistinctBoatSeats = new List<Boat>();
+            DistinctBoatSeats = boatseats.GroupBy(elem => elem.boatAmountSpaces).Select(g => g.First()).ToList();
+            return DistinctBoatSeats;
+        }
         // View boat details
         private void ViewBoat(object sender, RoutedEventArgs e)
         {
@@ -101,6 +150,42 @@ namespace KBSBoot.View
         private void BackToHomePage_Click(object sender, RoutedEventArgs e)
         {
             Switcher.Switch(new HomePageMember(FullName));
+        }
+
+        private void MenuFilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Opens and closes the filter popup
+            if (FilterPopup.IsOpen == false)
+            {
+                FilterPopup.IsOpen = true;
+            } else
+            {
+                FilterPopup.IsOpen = false;
+            }
+        }
+
+        private void SelectionFilteren_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ah niffo, werkt toch niet man.");
+        }
+
+        private void ResetSelection_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Deze ook niet.");
+            Bootplekken.IsEnabled = true;
+            StuurCheck.IsEnabled = true;
+            Bootnamen.IsEnabled = true;
+        }
+
+        private void Bootnamen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Bootplekken.IsEnabled = false;
+            StuurCheck.IsEnabled = false;
+        }
+
+        private void Bootplekken_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Bootnamen.IsEnabled = false;
         }
     }
 }
