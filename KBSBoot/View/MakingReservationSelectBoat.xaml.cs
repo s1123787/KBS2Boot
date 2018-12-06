507de5f04faad2e2b65614e1b7c58d55824f6452
unknown <rubenwezep@gmail.com>
2018-12-06 14:43:31 +0100

    changes to functionality of reservation

--- KBSBoot/View/MakingReservationSelectBoat.xaml.cs
+++ KBSBoot/View/MakingReservationSelectBoat.xaml.cs
@@ -25,6 +25,9 @@ namespace KBSBoot.View
         public string FullName;
         public int AccessLevel;
         public int MemberId;
+        private bool FilterEnabled = false;
+        private string bootnaam;
+        private int bootplek;
 
         public MakingReservationSelectBoat(string FullName, int AccessLevel, int MemberId)
         {
@@ -32,8 +35,97 @@ namespace KBSBoot.View
             this.AccessLevel = AccessLevel;
             this.MemberId = MemberId;
             InitializeComponent();
+
+            Bootplekken.ItemsSource = LoadBoatSeatsSelection();
+            Bootnamen.ItemsSource = LoadBoatNamesSelection();
+        }
+        private List<BoatTypes> LoadBoatNamesSelection()
+        {
+            List<BoatTypes> boatnames = new List<BoatTypes>();
+            using (var context = new BootDB())
+            {
+                var tableData = (from b in context.Boats
+                                 join bt in context.BoatTypes
+                                 on b.boatTypeId equals bt.boatTypeId
+                                 select new
+                                 {
+                                     boatNames = bt.boatTypeName
+                                 });
+
+                foreach (var b in tableData)
+                {
+                    boatnames.Add(new BoatTypes()
+                    {
+                        boatTypeName = b.boatNames
+                    });
+                }
+            }
+            List<BoatTypes> DistinctBoatSeats = new List<BoatTypes>();
+            DistinctBoatSeats = boatnames.GroupBy(elem => elem.boatTypeName).Select(g => g.First()).ToList();
+            return DistinctBoatSeats;
+        }
+        private List<BoatTypes> LoadBoatSeatsSelection()
+        {
+            List<BoatTypes> boatseats = new List<BoatTypes>();
+            using (var context = new BootDB())
+            {
+                var tableData = (from b in context.Boats
+                                 join bt in context.BoatTypes
+                                 on b.boatTypeId equals bt.boatTypeId
+                                 select new
+                                 {
+                                     boatAmountSpaces = bt.boatAmountSpaces
+                                 });
+
+                foreach (var b in tableData)
+                {
+                    boatseats.Add(new BoatTypes()
+                    {
+                        boatAmountSpaces = b.boatAmountSpaces
+                    });
+                }
+            }
+            List<BoatTypes> DistinctBoatSeats = new List<BoatTypes>();
+            DistinctBoatSeats = boatseats.GroupBy(elem => elem.boatAmountSpaces).Select(g => g.First()).ToList();
+            return DistinctBoatSeats;
+        }
+        private void SelectionFilteren_Click(object sender, RoutedEventArgs e)
+        {
+            //Reload the screen
+            FilterEnabled = true;
+            LoadBoats();
         }
 
+        private void ResetSelection_Click(object sender, RoutedEventArgs e)
+        {
+            //Reload the screen
+            FilterEnabled = false;
+            LoadBoats();
+            //BoatList.ItemsSource = LoadCollectionData();
+            //Resets the filteroptions
+            Bootplekken.IsEnabled = true;
+            Bootnamen.IsEnabled = true;
+            Bootnamen.SelectedItem = null;
+            Bootplekken.SelectedItem = null;
+        }
+
+        private void Bootnamen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Bootnamen.SelectedItem != null)
+            {
+
+                Bootplekken.IsEnabled = false;
+            }
+        }
+        private void Bootplekken_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Bootplekken.SelectedItem != null)
+            {
+                //Assigns value to chosen option
+                bootplek = Int32.Parse(Bootplekken.SelectedItem.ToString());
+                Bootnamen.IsEnabled = false;
+            }
+        }
         private void BackToHomePage_Click(object sender, RoutedEventArgs e)
         {
             if (AccessLevel == 1)
@@ -123,6 +215,26 @@ namespace KBSBoot.View
 
                 foreach (var d in data)
                 {
+                    //Filters selection based on chosen options
+                    if (FilterEnabled)
+                    {
+                        if (Bootnamen.SelectedItem != null)
+                        {
+                            bootnaam = Bootnamen.SelectedItem.ToString();
+                            if (d.boatType != bootnaam)
+                            {
+                                continue;
+                            }
+                        }
+                        if (Bootplekken.SelectedItem != null)
+                        {
+                            if (d.boatAmountSpaces != bootplek)
+                            {
+                                continue;
+                            }
+                        }
+                    }
+
                     string steer = (d.boatSteer == 0) ? "nee" : "ja";
 
                     boats.Add(new Boat(d.boatType, d.boatTypeDescription, d.boatAmountSpaces, steer) { boatId = d.boatId, boatName = d.boatName, boatTypeId = 1, boatOutOfService = 1, boatYoutubeUrl = null });
