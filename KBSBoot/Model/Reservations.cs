d7dd040545f619c26e66a82587f05fd762fcd926
youridekker <youridekker2000@hotmail.com>
2018-12-07 10:47:05 +0100

    moved method

--- KBSBoot/Model/Reservations.cs
+++ KBSBoot/Model/Reservations.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KBSBoot.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -40,6 +41,37 @@ namespace KBSBoot.Model
             this.endTime = endTime;
             this.beginTimeString = beginTime.ToString(@"hh\:mm");
             this.endTimeString = endTime.ToString(@"hh\:mm");
+        }
+
+        public void DeleteReservation(int reservationId)
+        {
+            List<Reservations> list = new List<Reservations>();
+            using (var context = new BootDB())
+            {
+                var data = (from r in context.Reservations
+                            where r.reservationId == reservationId
+                            select new { r.reservationId, r.memberId, r.date, r.beginTime, r.endTime });
+
+                foreach (var d in data)
+                {
+                    Reservations res1 = new Reservations(d.reservationId, d.memberId, d.date, d.beginTime, d.endTime);
+                    context.Reservations.Attach(res1);
+                    context.Reservations.Remove(res1);
+                }
+
+                var data2 = (from rb in context.Reservation_Boats
+                             where rb.reservationId == reservationId
+                             select new { rb.reservationId, rb.boatId });
+                foreach (var d in data2)
+                {
+                    Reservation_Boats reservation_Boats = new Reservation_Boats(d.reservationId, d.boatId);
+                    context.Reservation_Boats.Attach(reservation_Boats);
+                    context.Reservation_Boats.Remove(reservation_Boats);
+                }
+
+                context.SaveChanges();
+
+            }
         }
     }
 }
--- KBSBoot/View/ReservationsScreen.xaml.cs
+++ KBSBoot/View/ReservationsScreen.xaml.cs
@@ -191,44 +191,10 @@ namespace KBSBoot.View
             var result = MessageBox.Show($"Weet u zeker dat u de reservering van {reservation.resdate} om {reservation.beginTimeString} uur tot {reservation.endTimeString} uur wilt annuleren?", "Annuleren", MessageBoxButton.YesNo, MessageBoxImage.Asterisk);
             if(result == MessageBoxResult.Yes)
             {
-                DeleteReservation(reservation.reservationId);
+                reservation.DeleteReservation(reservation.reservationId);
                 Switcher.Switch(new ReservationsScreen(FullName, AccessLevel, MemberId));
             }
         }
-
-        private void DeleteReservation(int reservationId)
-        {
-            List<Reservations> list = new List<Reservations>();
-            using (var context = new BootDB())
-            {
-                var data = (from r in context.Reservations
-                            where r.reservationId == reservationId
-                            select new { r.reservationId, r.memberId, r.date, r.beginTime, r.endTime});
-
-                foreach (var d in data)
-                {
-                    Reservations res1 = new Reservations(d.reservationId, d.memberId, d.date, d.beginTime, d.endTime);
-                    context.Reservations.Attach(res1);
-                    context.Reservations.Remove(res1);
-                }
-
-                var data2 = (from rb in context.Reservation_Boats
-                             where rb.reservationId == reservationId
-                             select new {rb.reservationId, rb.boatId });
-                foreach(var d in data2)
-                {
-                    Reservation_Boats reservation_Boats = new Reservation_Boats(d.reservationId, d.boatId);
-                    context.Reservation_Boats.Attach(reservation_Boats);
-                    context.Reservation_Boats.Remove(reservation_Boats);
-                }
-
-                context.SaveChanges();
-                
-            }
-        }
-
-        
-
 
     }
 }
