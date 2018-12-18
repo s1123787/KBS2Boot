5bff335eab304cc44c3c5a583319564368a2e728
Mika <mikavandenbrink@outlook.com>
2018-12-18 14:02:33 +0100

    Merged Micks branch, then updated the filter-thing

--- KBSBoot/View/boatOverviewScreen.xaml.cs
+++ KBSBoot/View/boatOverviewScreen.xaml.cs
@@ -136,11 +136,20 @@ namespace KBSBoot.View
                                      on b.boatTypeId equals bt.boatTypeId
                                      select new
                                      {
+                                         boatId = b.boatId,
                                          boatNames = bt.boatTypeName
                                      });
                     //Fills list with all the typename options
                     foreach (var b in tableData)
                     {
+                        Boat boat = new Boat()
+                        {
+                            boatId = b.boatId
+                        };
+                        if(boat.CheckIfTodayBoatIsInMaintenance() == true)
+                        {
+                            continue;
+                        }
                         boatnames.Add(new BoatTypes()
                         {
                             boatTypeName = b.boatNames
@@ -172,11 +181,20 @@ namespace KBSBoot.View
                                      on b.boatTypeId equals bt.boatTypeId
                                      select new
                                      {
+                                         boatId = b.boatId,
                                          boatAmountSpaces = bt.boatAmountSpaces
                                      });
                     //Fills list with all the seat options
                     foreach (var b in tableData)
                     {
+                        Boat boat = new Boat()
+                        {
+                            boatId = b.boatId
+                        };
+                        if (boat.CheckIfTodayBoatIsInMaintenance() == true)
+                        {
+                            continue;
+                        }
                         boatseats.Add(new BoatTypes()
                         {
                             boatAmountSpaces = b.boatAmountSpaces
